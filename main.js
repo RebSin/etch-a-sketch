fb1540225c31c54455f12fbb812209d552b5e1e7
Rebecca <rebeccasingh98@gmail.com>
2020-05-20 17:27:46 -0700

    Added background change on hover

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ function createBoxes(userBoxSize) { //takes in the user input size
     for (let i = 0; i < userBoxSize; i++) { //column
         for (let j = 0; j < userBoxSize; j++) { //row
             const div = document.createElement('div'); //create div
-            div.addEventListener("mouseover", function () {
-                hoverFunction()
+            div.addEventListener("mouseover", function (e) { //e is the event
+                hoverFunction(e); //pass the event into the function
             });
             let boxSize = containerSize / userBoxSize; //divide the size of the container by number of boxes
             div.style.width = boxSize + "px"; // set the size of the boxes
@@ -20,8 +20,9 @@ function createBoxes(userBoxSize) { //takes in the user input size
     }
 }
 
-function hoverFunction () {
+function hoverFunction (e) {
     console.log ("Hey, I'm hovering!");
+    e.target.style.backgroundColor = "black"; //get the target of the event, change its background colour
 }
 
 createBoxes(50);
